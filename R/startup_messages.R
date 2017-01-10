fc955158a23696a06c6049f4fcdaa852306bea9b
Ari Lamstein <ari@lamsteinconsulting.com>
2017-01-10 15:03:56 +0000

    version 3.5.3

--- R/startup_messages.R
+++ R/startup_messages.R
@@ -1,12 +1,9 @@
 .onAttach <- function(...) {
-  if (!interactive() || stats::runif(1) > 0.1) return()
+  if (!interactive()) return()
   
   tips <- c(
-    "Stay up to date on choroplethr by following the blog: http://arilamstein.com/blog",
-    "Need help getting started? Try the free course 'Learn to Map Census Data in R': http://arilamstein.com/free-course/",
-    "Technical question? Try stack overflow: http://stackoverflow.com/questions/tagged/r",
-    "Want to take an in depth course on Choroplethr? Try 'Mapmaking in R with Choroplethr': http://courses.arilamstein.com/courses/mapmaking-r-choroplethr",
-    "Use suppressPackageStartupMessages() to eliminate package startup messages."
+    "View the Choroplethr documentation here: www.AriLamstein.com/open-source",
+    "Choroplethr has a free course: www.CensusMappingCourse.com"
   )
   
   tip <- sample(tips, 1)
