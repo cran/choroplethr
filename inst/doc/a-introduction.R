6a052d9c27d5d522538db7f5194ccb788b5907c9
Ari Lamstein <ari@lamsteinconsulting.com>
2024-03-02 00:52:36 +0000

    version 3.7.3

--- inst/doc/a-introduction.R
+++
@@ -1,8 +0,0 @@
-## ----message=FALSE------------------------------------------------------------
-    library(choroplethr)
-
-    data(df_pop_state)
-    state_choropleth(df_pop_state, 
-                     title  = "2012 US State Population Estimates", 
-                     legend = "Population")
-
